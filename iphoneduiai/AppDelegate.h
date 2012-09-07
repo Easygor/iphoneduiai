a6b0afadbc721d9b886c40bef354d9ba3dc712a5
cloud <cloudcry@gmail.com>
2012-09-07 15:34:32 +0800

    Initial Commit

---
+++ iphoneduiai/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  iphoneduiai
+//
+//  Created by Cloud Dai on 12-9-7.
+//  Copyright (c) 2012年 duiai.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
