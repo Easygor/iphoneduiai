da7740f63665b3e2051e3808426fb7886a7dd9d3
yliping <81168900@qq.com>
2012-10-02 20:20:29 +0800

    fix conflict

--- iphoneduiai/HZTimePickerView.h
+++ iphoneduiai/HZTimePickerView.h
@@ -2,32 +2,12 @@
 //  HZTimePickerView.h
 //  iphoneduiai
 //
-//  Created by yinliping on 12-10-1.
+//  Created by yinliping on 12-10-2.
 //  Copyright (c) 2012年 duiai.com. All rights reserved.
 //
 
 #import "BottomPopView.h"
-@class HZTimePickerView;
 
-@protocol HZDegreePickerDatasource <NSObject>
-
-- (NSArray *)timePickerData:(HZTimePickerView *)picker;
-
-@end
-
-@protocol HZTimePickerDelegate <NSObject>
-
-@optional
-- (void)timePickerDidChangeStatus:(HZTimePickerView*)picker withNum:(NSString*)num withDesc:(NSString *)desc;
-
-@end
-
-
-@interface HZTimePickerView : BottomPopView<UIPickerViewDataSource, UIPickerViewDelegate>
-@property (assign, nonatomic) id <HZTimePickerDelegate> delegate;
-@property (strong, nonatomic) IBOutlet UIPickerView *degreePicker;
-
--(id)initWithDelegate:(id <HZTimePickerDelegate>)delegate;
-
+@interface HZTimePickerView : BottomPopView
 
 @end
--- iphoneduiai/HZTimePickerView.m
+++ iphoneduiai/HZTimePickerView.m
@@ -2,60 +2,13 @@
 //  HZTimePickerView.m
 //  iphoneduiai
 //
-//  Created by yinliping on 12-10-1.
+//  Created by yinliping on 12-10-2.
 //  Copyright (c) 2012年 duiai.com. All rights reserved.
 //
 
 #import "HZTimePickerView.h"
-@interface HZTimePickerView ()
-
-@property (strong, nonatomic) NSArray *levels;
-@property (strong, nonatomic) NSDictionary *degrees;
-
-@end
 
 @implementation HZTimePickerView
-@synthesize degreePicker=_degreePicker;
-@synthesize delegate=_delegate;
-@synthesize levels=_levels, degrees=_degrees;
-
-- (void)dealloc
-{
-    self.delegate = nil;
-    [_levels release];
-    [_degrees release];
-    [_degreePicker release];
-    [super dealloc];
-}
-
--(NSArray *)levels
-{
-    if (_levels == nil) {
-        _levels = [[NSArray alloc] initWithObjects:@"高中以下", @"高中", @"大专", @"本科", @"硕士", @"博士", nil];
-    }
-    return _levels;
-}
-
--(NSDictionary *)degrees
-{
-    if (_degrees == nil) {
-        _degrees = [[NSDictionary alloc] initWithObjectsAndKeys:@"0", @"高中以下", @"1", @"高中", @"2", @"大专",
-                    @"3", @"本科", @"4", @"硕士", @"5", @"博士", nil];
-    }
-    
-    return _degrees;
-}
-
--(id)initWithDelegate:(id<HZTimePickerDelegate>)delegate
-{
-    self = [[[[NSBundle mainBundle] loadNibNamed:@"HZAreaPickerView" owner:self options:nil] objectAtIndex:2] retain];
-    if (self) {
-        self.delegate = delegate;
-    }
-    
-    return self;
-    
-}
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -66,40 +19,13 @@
     return self;
 }
 
-#pragma mark - PickerView lifecycle
-
--(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
 {
-    return 1;
+    // Drawing code
 }
-
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
-{
-    return self.levels.count;
-    
-}
-
-- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
-{
-    
-    return [self.levels objectAtIndex:row];
-    
-}
-
-- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
-{
-    NSLog(@"hahaha");
-    
-//    if([self.delegate respondsToSelector:@selector(dgreePickerDidChangeStatus:withNum:withDesc:)]) {
-//        [self.delegate dgreePickerDidChangeStatus:self withNum:[self.degrees objectForKey:[self.levels objectAtIndex:row]]
-//                                         withDesc:[self.levels objectAtIndex:row]];
-//    }
-//    
-//    if ([self.delegate respondsToSelector:@selector(timePickerDidChangeStatus:withNum:withDesc:)]) {
-//        [self.delegate timePickerDidChangeStatus:self withNum:[self.de] withDesc:<#(NSString *)#>]
-//    }
-    
-}
-
+*/
 
 @end
