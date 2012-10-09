25b659017f3f5d9015e519ade0328af7171322f5
yliping <81168900@qq.com>
2012-10-09 23:24:55 +0800

    changes

---
+++ iphoneduiai/choosePartnerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChoosePartnerViewController.h
+//  iphoneduiai
+//
+//  Created by yinliping on 12-10-8.
+//  Copyright (c) 2012年 duiai.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChoosePartnerViewController : UITableViewController
+
+@end
